7bf34c9b63a478a8c87a21492e13714963ed5b5e
JeremyEngleman-Dev <wellsendit@gmail.com>
2024-06-24 21:43:45 -0500

    Added chapter 2

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
     
 class TestHTMLNode(unittest.TestCase):
     def test_props(self):
@@ -13,5 +13,40 @@ class TestHTMLNode(unittest.TestCase):
         test = HTMLNode(None, None, None, None)
         self.assertEqual(test.props_to_html(), '')
 
+    def test_to_html_no_children(self):
+        test = LeafNode("p", "Im a leaf on the wind")
+        self.assertEqual(test.to_html(), '<p>Im a leaf on the wind</p>')
+    def test_to_html_no_tag(self):
+        test = LeafNode(None, "Leafy bits")
+        self.assertEqual(test.to_html(), "Leafy bits")
+
+    def test_to_html_many_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ]
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+    def test_headings(self):
+        node = ParentNode(
+            "h2",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>",
+        )
 if __name__ == "__main__":
     unittest.main()
